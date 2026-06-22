8ae30181bcb0a940210d0bd06f50ed65c204e85c
Tashbme005 <shatrahddamulira005@gmail.com>
2026-06-22 10:17:28 +0300

    added a new file

---
+++ operators.py
@@ -0,0 +1,36 @@
+a=7
+b=3
+print("sum",a+b)
+print("floor div", a//b)
+print("modulo", a%b)
+print("power",a**b)
+a+=7
+print(a)
+print(a+b)
+print(a*b)
+a-=7
+print(a)
+a*=2
+print(a)
+a/=2
+print(a)
+a%=3
+print(a)
+a**=7
+print(a)
+print(a==b)
+x=a
+print(x)
+y=b
+print(y)
+print(x==y)
+print(x!=y)
+print(x>y)
+print(y>x)
+print(x<y)
+print(x>=y)
+print(x<=y)
+print(x<5 and y<5)
+print(x<1 or y>1)
+z=[10,20,30,40]
+print(10 in z) 
